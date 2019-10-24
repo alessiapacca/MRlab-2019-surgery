733c84e2a2114e040088a6fec37e81f318a4c637
Zephyrl <3150104024@zju.edu.cn>
2019-10-24 12:48:03 +0200

    add handmenu

--- Assets/MyDemo/Assets/Scripts/ResetPositions.cs
+++ Assets/MyDemo/Assets/Scripts/ResetPositions.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class ResetPositions : MonoBehaviour
 {
-    private List<GameObject> bones = new List<GameObject>();
-    private List<Transform> transformOfBones = new List<Transform>();
-    private ReadOnlyCollection<Transform> transformOfBonesCopy;
+    class TransformInfo
+    {
+        public Vector3 pos;
+        public Quaternion rotate;
+        public Vector3 scale;
+    }
 
+    private static List<GameObject> bones = new List<GameObject>();
+    //private static List<Transform> transformOfBones = new List<Transform>();
+
+    private static List<TransformInfo> originalTransform = new List<TransformInfo>();
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +25,17 @@ public class ResetPositions : MonoBehaviour
             GameObject t = GameObject.Find("Bone_" + i);
             bones.Add(t);
             Transform tr = t.GetComponent<Transform>();
+            TransformInfo ti = new TransformInfo
+            {
+                pos = tr.localPosition,
+                rotate = tr.localRotation,
+                scale = tr.localScale
+            };
             //Debug.Log(tr);
-            transformOfBones.Add(tr);
+            originalTransform.Add(ti);
         }
         Debug.Log("Init: #bones, " + bones.Count);
-        Debug.Log("Init: #transforms, " + transformOfBones.Count);
-
-        //transformOfBonesCopy = new ReadOnlyCollection<Transform>(transformOfBones);
-        //Debug.Log("Num of Bones1 , " + transformOfBonesCopy.Count);
+        Debug.Log("Init: #transforms, " + originalTransform.Count);
 
     }
 
@@ -42,13 +52,16 @@ public class ResetPositions : MonoBehaviour
 
         Debug.Log("Botton Pressed");
 
-        Debug.Log("Num of Bones, " + transformOfBones.Count);
+        //Debug.Log("Num of Bones transform, " + transformOfBones.Count);
+        Debug.Log("Num of Bonesbones, " + bones.Count);
 
-        for (int i = 1; i <=6 ; i++)
+        Transform worldTransform = GameObject.Find("BoneCollection").transform;
+
+        for (int i = 0; i < 6; i++)
         {
-            //Debug.Log(transformOfBones[i]);
-            Vector3 pos = GameObject.Find("BoneCollection").transform.position;
-            GameObject.Find("Bone_"+i).transform.SetPositionAndRotation(pos, new Quaternion(0, 0, 0, 1));
+            bones[i].transform.localPosition = originalTransform[i].pos;
+            bones[i].transform.localRotation = originalTransform[i].rotate;
+            bones[i].transform.localScale = originalTransform[i].scale;
         }
 
     }
